ef769ec964e13ea67d8ebabecff7502b3e796f98
jessicacliong <14395@coderacademy.edu.au>
2023-12-08 16:11:31 +1100

    feat: nodemon installed and command written

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const express = require('express');
 // make an instance of the server that we can customise and run
 const app = express();
 
+// Allow server flexibility to open on other ports if occupied
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+
 // GET localhost:3000/
 // app.get(route path, callback_function)
 app.get("/", (request, response) => {
 
+     response.send('Hello World!');
+
 });
 
-app.listen(3000,() => {
-     console.log("Server is running on port: " + 3000);
-})
+app.listen(PORT, HOST,() => {
+     console.log("Server is running on port: " + PORT);
+});
