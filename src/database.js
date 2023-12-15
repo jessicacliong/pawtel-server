7662ae13a75c05c36287e2c0f9c36450c9cf88ff
jessicacliong <14395@coderacademy.edu.au>
2023-12-15 16:39:45 +1100

    feat: connected to atlas mongodb

--- src/database.js
+++ src/database.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 async function databaseConnect(){
      try {
-          await mongoose.connect('')
+          await mongoose.connect(process.env.DB_URI)
           console.log("Database connected");
      } catch (error) {
           console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
