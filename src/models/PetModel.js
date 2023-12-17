b9526cb8cf31787b408ac1b71694d21bc1358b4d
jessicacliong <14395@coderacademy.edu.au>
2023-12-17 19:02:56 +1100

    feat: created pet, booking and user schemas and implemented all models

---
+++ src/models/PetModel.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PetSchema = new Schema({
+     name: {
+          type: String,
+          required: true,
+          unique: false
+     },
+     animalType: {
+          type: String,
+          required: true,
+          unique: false
+     },
+     breed: {
+          type: String,
+          required: true,
+          unique: false
+     },
+	colour: {
+          type: String,
+          required: true,
+          unique: false
+     },
+     gender: {
+          type: String,
+          required: false,
+          unique: false
+     },
+     age: {
+          type: Number,
+          required: false,
+          unique: false
+     },
+     favouriteToys: {
+          type: [String],
+          required: false,
+          unique: false
+     },
+     dietaryRequirements: {
+          type: [String],
+          required: false,
+          unique: false
+     },
+     allergies: {
+          type: [String],
+          required: false,
+          unique: false
+     }
+});
+
+PetSchemaSchema.pre(
+     'save',
+     async function (next) {
+          console.log("About to save a pet to PawtelDB");
+          next();
+     }
+)
+
+const Pet = mongoose.model('Pet', PetSchema)
+
+module.exports = {
+     Pet
+}
+
+
+/*   const Pet = mongoose.model(`Pet`, {
+     name: String,
+     animalType: String,
+     breed: String,
+     colour: String,
+     gender: String,
+     age: Number,
+     favouriteToys: [String],
+     dietaryRequirements: [String],
+     allergies: [String],
+});
+
+*/
