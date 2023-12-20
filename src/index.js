83de8b9faf5c9b0d4828ecf824e140a21f999459
jessicacliong <14395@coderacademy.edu.au>
2023-12-20 17:38:32 +1100

    fix: server uses PORT variable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const { databaseConnect } = require('./database');
 const { app } = require('./server');
 
-app.listen(3030, async () => {
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, async () => {
      await databaseConnect();
      console.log('Pawtel server is running!');
 
